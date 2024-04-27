af5a71e9938b5aa4775434d835e88447d2f0fc1e
Muhammad Affan <maffan2202@gmail.com>
2024-04-27 23:59:45 +0500

    initial commit

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+                <a href="dummy">Home</a>
+                <a href="dummy">Create New Blog</a>
+                <h1>Hello World</h1>
+            </div>
+        </nav>
+
+    );
+}
+
+export default Navbar;
